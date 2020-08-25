fcf82fd4d873f2814bc013819323b63f5092ca26
Yevhen Havrasiienko <evgengavrasienko@gmail.com>
2020-08-25 18:33:34 +0300

    Added core logic for the Knights on a Board task and unit tests

--- Infrascale.Interview.Task/KnightsOnABoard.cs
+++ Infrascale.Interview.Task/KnightsOnABoard.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Infrascale.Interview.Task
 {
@@ -15,9 +16,70 @@ namespace Infrascale.Interview.Task
         */
         #endregion
 
-        public bool CheckThatKnigthCantCaptureAnother()
+        /// <summary>
+        /// As I understood, so that the knight could not capture another,
+        /// there should be only zeros in front of him and near him
+        /// </summary>
+        /// <returns></returns>
+        public bool CheckThatKnigthCantCaptureAnother(int[,] chessPlate)
         {
-            return false;
+            var positionsOfKnigths = new List<Knigth>(); // to save all of the knigth positions
+
+            for (var i = 0; i < 8; i++)
+            {
+                // Knights can be present in any of the 64 squares ==> 8 elements in each row
+                for (var j = 0; j < 8; j++)
+                {
+                    if(chessPlate[i, j] == 1) // save all the knigths with their position
+                    {
+                        positionsOfKnigths.Add(new Knigth { PositionX = i,PositionY = j });
+                    }
+                }
+            }
+
+            // Check
+            foreach (var position in positionsOfKnigths)
+            {
+                if (position.PositionX != 0) // first row
+                {
+                    if (chessPlate[position.PositionX - 1, position.PositionY] != 0)
+                    {
+                        return false;
+                    }
+                }
+
+                if (position.PositionY != 0) // its not the first element in the row
+                {
+                    if (chessPlate[position.PositionX, position.PositionY - 1] != 0)
+                    {
+                        return false;
+                    }
+                }
+
+                if (position.PositionY < 7) // its not the last element in the row
+                {
+                    if (chessPlate[position.PositionX, position.PositionY + 1] != 0)
+                    {
+                        return false;
+                    }
+                }
+
+                if (position.PositionX < 7)
+                {
+                    if (chessPlate[position.PositionX + 1, position.PositionY] != 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
         }
+    }
+
+    public class Knigth
+    {
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
     }
 }
--- Infrascale.Interview.Test/UnitTests/KnightsOnABoardTests.cs
+++ Infrascale.Interview.Test/UnitTests/KnightsOnABoardTests.cs
@@ -1,18 +1,79 @@
+using Infrascale.Interview.Task;
 using NUnit.Framework;
 
 namespace Infrascale.Interview.Test
 {
-    public class Tests
+    [TestFixture]
+    public class KnightsOnABoardTests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void CheckThatKnigthCantCaptureAnother_ValidCase()
         {
+            // Arrange
+            var chessPlateOne = new int[,] {
+              { 0, 0, 0, 1, 0, 0, 0, 0 },
+              { 0, 0, 0, 0, 0, 0, 0, 0 },
+              { 0, 1, 0, 0, 0, 1, 0, 0 },
+              { 0, 0, 0, 0, 1, 0, 1, 0 },
+              { 0, 1, 0, 0, 0, 1, 0, 0 },
+              { 0, 0, 0, 0, 0, 0, 0, 0 },
+              { 0, 1, 0, 0, 0, 0, 0, 1 },
+              { 0, 0, 0, 0, 1, 0, 0, 0 }
+            };
+            var chessPlateTwo = new int[,] {
+              { 0, 0, 0, 1, 0, 0, 0, 0 },
+              { 0, 0, 0, 0, 0, 0, 0, 1 },
+              { 0, 1, 0, 0, 0, 1, 0, 0 },
+              { 1, 0, 0, 0, 1, 0, 1, 0 },
+              { 0, 1, 0, 0, 0, 1, 0, 0 },
+              { 0, 0, 0, 0, 1, 0, 0, 0 },
+              { 0, 1, 0, 0, 0, 0, 0, 1 },
+              { 0, 0, 0, 0, 1, 0, 0, 0 }
+            };
+            var sut = new KnightsOnABoard();
+
+            // Act
+            var resultOne = sut.CheckThatKnigthCantCaptureAnother(chessPlateOne);
+            var resultTwo = sut.CheckThatKnigthCantCaptureAnother(chessPlateTwo);
+
+            // Assert
+            Assert.That(resultOne == true);
+            Assert.That(resultTwo == true);
         }
 
         [Test]
-        public void Test1()
+        public void CheckThatKnigthCantCaptureAnother_WrongCase()
         {
-            Assert.Pass();
+            // Arrange
+            var chessPlateOne = new int[,] {
+              {1, 0, 1, 0, 1, 0, 1, 0},
+              {0, 1, 0, 1, 0, 1, 0, 1},
+              {1, 0, 1, 0, 1, 0, 1, 0},
+              {0, 0, 0, 1, 0, 1, 0, 1},
+              {1, 0, 0, 0, 1, 0, 1, 0},
+              {0, 0, 0, 0, 0, 1, 0, 1},
+              {1, 0, 1, 0, 1, 0, 1, 0},
+              {1, 0, 0, 1, 0, 0, 0, 1}
+            };
+            var chessPlateTwo = new int[,] {
+              {1, 0, 1, 0, 1, 0, 1, 0},
+              {0, 1, 0, 1, 0, 1, 0, 1},
+              {1, 0, 1, 0, 1, 0, 1, 0},
+              {0, 0, 0, 1, 0, 1, 0, 1},
+              {1, 0, 1, 0, 1, 0, 1, 0},
+              {0, 0, 0, 0, 0, 1, 0, 1},
+              {0, 0, 1, 0, 1, 0, 1, 0},
+              {1, 0, 0, 1, 1, 0, 0, 1}
+            };
+            var sut = new KnightsOnABoard();
+
+            // Act
+            var firstResult = sut.CheckThatKnigthCantCaptureAnother(chessPlateOne);
+            var secondResult = sut.CheckThatKnigthCantCaptureAnother(chessPlateTwo);
+
+            // Assert
+            Assert.That(firstResult == false);
+            Assert.That(secondResult == false);
         }
     }
 }
