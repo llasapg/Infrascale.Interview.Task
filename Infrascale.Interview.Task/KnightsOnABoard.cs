3a033ea5f1b8daaafa96e55207aea39ef914470f
Yevhen Havrasiienko <evgengavrasienko@gmail.com>
2020-08-25 17:13:11 +0300

    Added main project structure

---
+++ Infrascale.Interview.Task/KnightsOnABoard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Infrascale.Interview.Task
+{
+    /// <summary>
+    /// Class to represent the task "Knights On A Board"
+    /// </summary>
+    public class KnightsOnABoard
+    {
+        #region Task description
+        /*
+        Write a function that returns true if the knights are placed on a chessboard
+        such that no knight can capture another knight.
+        Here, 0s represent empty squares and 1s represent knights.
+        */
+        #endregion
+
+        public bool CheckThatKnigthCantCaptureAnother()
+        {
+            return false;
+        }
+    }
+}
